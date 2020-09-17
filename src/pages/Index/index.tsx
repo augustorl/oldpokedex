401f16006b483e919472e0676a6eed671e8d8f0b
augustorl <gutin22@gmail.com>
2020-09-17 17:25:49 -0300

    removing yup

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -1,5 +1,34 @@
-import React from 'react';
+import React, {useRef, useCallback} from 'react';
 import { FiSearch } from 'react-icons/fi';
 import {Form} from '@unform/web';
 import {FormHandles} from '@unform/core';
-import * as Yup from 'yup'; 
+import { useToast } from '../../hooks/Toast';
+import getValidationErrors from '../../utils/getValidationErrors';
+import logo from '../../assets/logo.svg';
+import Input from '../../components/Input';
+
+import { Container} from './styles';
+
+interface PokemonFormData {
+    pokemon: string;
+}
+
+const Index: React.FC = () => {
+    const formRef = useRef<FormHandles>(null);
+
+    const { addToast } = useToast();
+
+    return  (
+        <Container>
+            <img src={logo} alt="Pokemon" />
+            <Form ref={formRef} onSubmit={handleSubmit}>
+                <Input
+                    name="pokemon"
+                    icon={FiSearch}
+                    type="text"
+                    placeholder="Search Pokemon"
+                />
+            </Form>
+        </Container>
+    )
+}
