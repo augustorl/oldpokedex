580e4815419872ff26c7f3311b8640321a768910
augustorl <gutin22@gmail.com>
2020-09-21 17:53:10 -0300

    updating index styles

--- src/pages/Index/index.tsx
+++ src/pages/Index/index.tsx
@@ -3,31 +3,38 @@ import { FiSearch } from 'react-icons/fi';
 import {Form} from '@unform/web';
 import {FormHandles} from '@unform/core';
 import { useToast } from '../../hooks/Toast';
-import logo from '../../assets/logo.png';
+import logo from '../../assets/logo.svg';
 import Input from '../../components/Input';
 
-import { Container } from './styles';
-
+import { Container, FormContainer } from './styles';
+import api from '../../services/api';
 interface PokemonFormData {
     pokemon: string;
 }
 
 const Index: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
-
     const { addToast } = useToast();
 
+    const handleSubmit = useCallback(
+        async (data: PokemonFormData) => {
+            const response = await api.get('pokemons', {
+                data,
+            });
+        }, []);
     return  (
         <Container>
             <img src={logo} alt="Pokemon" />
+            <FormContainer>
             <Form ref={formRef} onSubmit={handleSubmit}>
                 <Input
                     name="pokemon"
-                    icon={FiSearch}
                     type="text"
                     placeholder="Search Pokemon"
+                    icon={FiSearch}
                 />
             </Form>
+            </FormContainer>
         </Container>
     )
 }
--- src/pages/Index/styles.tsx
+++ src/pages/Index/styles.tsx
@@ -2,5 +2,17 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   height: 100vh;
-  display: flex;;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+`;
+
+export const FormContainer = styled.div`
+display: flex;
+justify-content: center;
+form {
+  margin-top: 109px;
+  width: 560px;
+}
 `;
