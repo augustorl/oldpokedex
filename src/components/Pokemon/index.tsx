625955f0ec394296c2d1aceb03d6164fd23c8d96
augustorl <gutin22@gmail.com>
2020-09-30 00:46:23 -0300

    mocking up poke infos

--- src/components/Pokemon/index.tsx
+++ src/components/Pokemon/index.tsx
@@ -1,43 +1,61 @@
-import React, { Component } from 'react';
-import './stlyes';
+import React, {
+  useEffect,
+  useRef,
+  useState,
+  useCallback,
+} from 'react';
+import { Container } from  './styles';
 
-class Pokemon extends Component {
-	render() {
-		return (
-			<div id="pokedex">
-				<div id="pokedex-left">
-					<div id="pokemon-types"></div>
-                        <figure id="sprite-container">
-                            <img src={this.props.spriteUrl} className="sprite-image"/>
-                        </figure>
-						<div id="name-container">
-                            <span className="pokemon-name">{this.props.name}</span>
-                            <span className="pokemon-number">No.</span> {this.props.id}</span>
-                        </div>
-                        <div id="pokemon-description">
-                            <p>{this.props.description}</p>
-                        </div>
-                </div>
+interface PokeProps {
+  id: number;
+  type: string;
+  name: string;
+  description: string;
+  height: number;
+  weight: number;
+  hpbar: number;
+  attack: number;
+  deffense: number;
+  speed: number;
+  speedattack: number;
+  speeddeffense: number;
+}
+
+const Pokemon: React.FC = () => {
+  return (
+      <Container>
+        <div id="pokedex-left">
+					<div id="pokemon-types">
+            <p>Grass</p>
+            <p>Poison</p>
+          </div>
+          <figure id="sprite-container">
+            <img src="https://static.pokemonpets.com/images/monsters-images-300-300/1-Bulbasaur.png" className="sprite-image"/>
+          </figure>
+					<div id="name-container">
+              <span className="pokemon-name">Bulbasaur</span>
+              <span className="pokemon-number">No.</span> <span>1</span>
+          </div>
+          <div id="pokemon-description">
+            <p>A strange seed was planted on its back at birth. The plant sprouts and grows with this PokéMon.</p>
+          </div>
+        </div>
 					<div id="pokedex-right">
-
 						<div id="main-stats">
-							<div className="height"></div>
-							<div className="weight"></div>
-							<div className="hp-bar"><div>
+							<p className="height">10</p>
+							<p className="weight">98</p>
+							<div className="hp-bar"><p>10</p></div>
 						</div>
 
 						<div id="base-stats">
-							<div className="attack"></div>
-							<div className="deffense"></div>
-							<div className="speed"></div>
-							<div className="speed-attack"></div>
-							<div className="speed-deffense"></div>
+							<div className="attack"><h1>attack:</h1><p>20</p></div>
+							<div className="deffense"></div><h1>defense</h1><p>30</p>
+							<div className="speed"></div><h1>speed</h1><p>20</p>
+							<div className="speed-attack"></div><h1>sp. att.</h1><p>30</p>
+							<div className="speed-deffense"></div><h1>sp. def.</h1><p>40</p>
 						</div>
 					</div>
-                </div>
-			</div>
-	    )
-	}
-}
-
+      </Container>
+  );
+};
 export default Pokemon;
---
+++ src/components/Pokemon/styles.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+import pokedex from '../../assets/pokedex.png';
+export const Container = styled.div`
+background: url(${pokedex});
+`;
