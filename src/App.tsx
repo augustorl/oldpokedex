24ffbf11762623c39abbb003bb23aea44d909a6d
augustorl <gutin22@gmail.com>
2020-09-30 00:46:47 -0300

    pokemon on index

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
 import Index from './pages/Index';
+import Pokemon from './components/Pokemon';
+
 
 
 const App: React.FC = () => (
   <>
-    <Index/>
+    <Pokemon/>
     <GlobalStyle />
   </>
 );
