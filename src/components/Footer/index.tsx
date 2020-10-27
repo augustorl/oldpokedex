963b251bf0c18edefc2420af0d4a358151bbe549
Augusto Leite <43501682+augustorl@users.noreply.github.com>
2020-10-27 20:59:18 -0300

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,7 +7,7 @@ export default function Footer() {
     <Container>
       <footer>
           <div>
-          <p>Built with  ❤️ by <a href="https://augustoleite.com">@augustorl</a> © 2020 Copyright.</p>
+          <p>Built with   <span role="img" aria-label="heart">❤️</span> by <a href="https://augustoleite.com">@augustorl</a> © 2020 Copyright.</p>
           </div>
       </footer>
     </Container>
